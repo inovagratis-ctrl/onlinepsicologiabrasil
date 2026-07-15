20249c478b7508fd5d07efc3a0b109f4e37fdcc9
Eusimar <inovagratis@gmail.com>
2026-07-15 13:24:27 -0500

    fix: logo header 200x60 (horizontal)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           <Link href="/" className="flex items-center gap-2">
-            <Image src="/logo_completa_base.png" alt="Psicologia Direta" width={100} height={100} className="rounded-lg" unoptimized />
+            <Image src="/logo_completa_base.png" alt="Psicologia Direta" width={200} height={60} className="rounded-lg" unoptimized />
           </Link>
 
           <div className="hidden md:flex items-center gap-8">
